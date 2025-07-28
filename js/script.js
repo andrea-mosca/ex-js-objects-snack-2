f9ba9346147ab1aea8ce9cab9d598f736a0e04b6
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-28 13:16:16 +0200

    deepCopy function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-// **FUNZIONI DENTRO ALTRE FUNZIONI**
+// Crea una funzione che permette la copia profonda (deep copy) di un oggetto,
+//  che copia anche i suoi metodi (proprietà che contengono funzioni).
+// Usa l’oggetto di Code Question 6 come test.
+//  Serve usare una funzione ricorsiva! (fai un po’ di ricerca).
+const chef = {
+  name: "Chef Hyur",
+  age: 29,
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+  restaurant: {
+    name: "Hyur's Burgers",
+    welcomeClient: () => {
+      console.log("Benvenuto!");
+    },
+    address: {
+      street: "Main Street",
+      number: 123,
+      showAddress: () => {
+        console.log("Main Street 123");
+      },
+    },
+    isOpen: true,
+  },
+};
 
-function funzioneEsempio(prima, dopo) {
-  prima();
-  setTimeout(() => {
-    console.log("sono passati 1000ms");
-    dopo();
-  }, 1000);
+function deepCopy(obj) {
+  if (typeof obj !== `object`) obj;
+
+  if (Array.isArray(obj)) {
+    const copyArr = [];
+    for (let i = 0; i < obj.length; i++) {
+      copyArr[i] = deepCopy(obj[i]);
+    }
+    return copyArr;
+  }
+
+  const copy = {};
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value !== `object`) {
+      copy[key] = value;
+    } else copy[key] = deepCopy(value);
+  }
+  return copy;
 }
-funzioneEsempio(
-  () => console.log("aspetto"),
-  () => console.log("fine")
-);
-// *GENERATORE DI FUNZIONI
-function createGreeter(greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-}
-const sayHello = createGreeter("hello");
-sayHello("Andrea");
 
-// funzione che genera una funzione che moltiplica un numero
-
-function generateMultiplier(multiplier) {
-  return function (number) {
-    return number * multiplier;
-  };
-}
-
-const double = generateMultiplier(2);
-console.log(double(5));
+const chefCopy = deepCopy(chef);
+console.log(chefCopy);
